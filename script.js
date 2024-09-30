2a324beb43ea0da301d776421645543ce430f03d
Sivakumar-TL <165304998+Sivakumar-TL@users.noreply.github.com>
2024-09-30 18:07:28 +0530

    claculator

---
+++ script.js
@@ -0,0 +1,39 @@
+var value1 = document.getElementById("input1");
+var value2 = document.getElementById("input2");
+
+function add(){
+
+    var result = parseFloat(value1.value)+ parseFloat(value2.value);
+    document.getElementById("result").innerHTML = result;
+
+    clear();
+}
+
+function sub(){
+
+    var result = parseFloat(value1.value)+ parseFloat(value2.value);
+    document.getElementById("result").innerHTML = result;
+
+    clear();
+}
+
+function mul(){
+
+    var result = parseFloat(value1.value) + parseFloat(value2.value);
+    document.getElementById("result").innerHTML = result;
+
+    clear();
+}
+
+function div(){
+
+    var result = parseFloat(value1.value) + parseFloat(value2.value);
+    document.getElementById("result").innerHTML = result;
+
+    clear();
+}
+
+function clear(){
+    value1.value = "";
+    value2.value = "";
+}
